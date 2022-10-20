68a5b83ce73a7becc0083996abce439e4da47807
叶继伟 <729721503@qq.com>
2022-10-20 23:44:29 +0800

    redux配置

---
+++ src/store/modules/home.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const homeSlice = createSlice({
+  name: "home",
+  initialState: {
+    messgae: 'hello'
+  },
+  reducers: {
+
+  }
+})
+
+export default homeSlice.reducer
