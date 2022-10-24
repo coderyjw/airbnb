6fb59ec06405247e46bee436f4404677a4675ddd
叶继伟 <yejw6@asiainfo.com>
2022-10-24 18:03:07 +0800

    首页数据获取

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,28 +1,78 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { getHomeGoodPriceData } from "@/services/modules/home";
+import {
+  getHomeDiscountData,
+  getHomeGoodPriceData,
+  getHomeHighScoreData,
+  getHomeHotRecommendData,
+  getHomeLongforData,
+  getHomePlusData,
+} from "@/services";
 
-export const fetchHomeDataAction = createAsyncThunk("fetchData", async () => {
-  const res = await getHomeGoodPriceData();
-  return res;
+export const fetchHomeDataAction = createAsyncThunk("fetchData", (payload, { dispatch }) => {
+  getHomeGoodPriceData().then((res) => {
+    dispatch(changeGoodPriceInfoAction(res));
+  });
+  getHomeHighScoreData().then((res) => {
+    dispatch(changeHighScoreInfoAction(res));
+  });
+  getHomeDiscountData().then((res) => {
+    dispatch(changeDiscountInfoAction(res));
+  });
+  getHomeHotRecommendData().then((res) => {
+    dispatch(changeRecommendInfoAction(res));
+  });
+  getHomeLongforData().then((res) => {
+    dispatch(changeLongforInfoAction(res));
+  });
+  getHomePlusData().then((res) => {
+    dispatch(changePlusInfoAction(res));
+  });
 });
 
 const homeSlice = createSlice({
   name: "home",
   initialState: {
     goodPriceInfo: {},
+    highScoreInfo: {},
+    discountInfo: {},
+    recommendInfo: {},
+    longforInfo: {},
+    plusInfo: {},
   },
   reducers: {
     changeGoodPriceInfoAction(state, { payload }) {
       state.goodPriceInfo = payload;
     },
+    changeHighScoreInfoAction(state, { payload }) {
+      state.highScoreInfo = payload;
+    },
+    changeDiscountInfoAction(state, { payload }) {
+      state.discountInfo = payload;
+    },
+    changeRecommendInfoAction(state, { payload }) {
+      state.recommendInfo = payload;
+    },
+    changeLongforInfoAction(state, { payload }) {
+      state.longforInfo = payload;
+    },
+    changePlusInfoAction(state, { payload }) {
+      state.plusInfo = payload;
+    },
   },
   extraReducers: {
-    [fetchHomeDataAction.fulfilled](state, { payload }) {
-      state.goodPriceInfo = payload;
-    },
+    // [fetchHomeDataAction.fulfilled](state, { payload }) {
+    //   state.goodPriceInfo = payload
+    // }
   },
 });
 
-export const { changeGoodPriceInfoAction } = homeSlice.actions;
+export const {
+  changeGoodPriceInfoAction,
+  changeHighScoreInfoAction,
+  changeDiscountInfoAction,
+  changeRecommendInfoAction,
+  changeLongforInfoAction,
+  changePlusInfoAction,
+} = homeSlice.actions;
 
 export default homeSlice.reducer;
